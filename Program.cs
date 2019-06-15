a65d20b8798ebe2faeeee00487feacbde4a300b7
Jacob Byerline <47374239+jbyerline@users.noreply.github.com>
2019-06-14 17:19:30 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace SumOfDigits
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            // Declare Variables
+            int sum = 0;
+
+            // Print instructions to console
+            Console.WriteLine("Sum Of Digits");
+            Console.WriteLine("Enter Number: ");
+
+            // Collect input from console into String
+            string input = Console.ReadLine();
+
+            // Convert String to an array of Int
+            var intArray = input.Select(c => c - '0').ToArray();
+
+            // Sum up the numbers in the array
+            for(int i = 0; i <= intArray.Length - 1; i++)
+            {
+                sum += intArray[i];
+            }
+
+            // Print the answer
+            Console.WriteLine("The sum of the digits in: " + input + " is: " + sum);
+
+        }
+    }
+}
